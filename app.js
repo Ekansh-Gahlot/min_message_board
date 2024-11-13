eeff75faf759fc3e78e5bb4eac3ae838d27ff901
Ekansh-Gahlot <60690453+Ekansh-Gahlot@users.noreply.github.com>
2024-11-13 12:56:57 +0530

    add script for railway

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const indexRouter = require('./Routes/indexRouter.js')
 const messageRouter = require('./Routes/newMessageRouter.js')
 const app = express();
 const assetsPath = path.join(__dirname, "public");
+const http = require('http');
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -16,7 +17,10 @@ app.use('/new',messageRouter)
 app.use('/',indexRouter);
 
 
-
 app.listen(PORT,()=>{
     console.log(`App running on port ${PORT}`)
 })
+
+http.createServer(function(req,res){
+    req.send('Hello World')
+}).listen(8080)
