cec77b2a9445087ff100c81b05b8ce3bae358d4f
Ekansh-Gahlot <60690453+Ekansh-Gahlot@users.noreply.github.com>
2024-11-13 13:08:08 +0530

    fix script name

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const path = require('path');
 const indexRouter = require('./Routes/indexRouter.js')
 const messageRouter = require('./Routes/newMessageRouter.js')
 const app = express();
-const assetsPath = path.join(__dirname, "public");
+// const assetsPath = path.join(__dirname, "public");
 const http = require('http');
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static(assetsPath));
+// app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3000;
 
-const PORT = 3000;
 
 app.use('/new',messageRouter)
 app.use('/',indexRouter);
